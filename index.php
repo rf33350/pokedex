5e57c1383d4dfbcef22047a50ef8e30e92071595
Rémi Faure <djaroul@hotmail.fr>
2021-10-06 22:12:02 +0200

    change pika to bulbi

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
         <div class="card m-5" style="width: 18rem;">
             <img src="..." class="card-img-top" alt="Bulbizarre">
             <div class="card-body">
-                <h5 class="card-title">Pikachu</h5>
+                <h5 class="card-title">Bulbizarre</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
                     card's content.</p>
                 <a href="#" class="btn btn-warning">Modifier</a>
